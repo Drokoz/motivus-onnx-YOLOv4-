c84e17e72a3aebe7aae3a8666a831569e52422d6
Tomás Herceg <49624623+Drokoz@users.noreply.github.com>
2022-08-31 14:13:45 -0400

    Proccesing bach finished
    The model has to be pre load and with an pre execution

--- ONNX/public/index.js
+++ ONNX/public/index.js
@@ -11,8 +11,8 @@ function download(content, fileName, contentType) {
  }
  
 //Function to jsonfy output and download
-function onDownload(jsonData){
-download(JSON.stringify(jsonData), "output.json", "text/plain");
+function onDownload(jsonData,fileName){
+download(JSON.stringify(jsonData), fileName, "text/plain");
 }
 
 // Create an ONNX inference session with WebGL backend.
@@ -38,7 +38,7 @@ async function imgSet() {
 async function runExample() {
 
   //This commented section is for another preprocess steps
-    // var img = document.getElementById("image_url")
+    //var img = document.getElementById("image_url")
     
     const img = new Image();
     img.onload = function() {
@@ -58,25 +58,36 @@ async function runExample() {
     const width = imageSize;
     const height = imageSize;
 
-    start_time = performance.now();
+    
     //debugSlice()
-    const preprocessedData = preprocessYOLO(imageData.data, width, height, img);
+    const preprocessedData = preprocessYOLO(width, height, img);
+
+    console.log("first", preprocessedData)
     //console.log([1, width, height, 3])
 
-    const inputTensor = new ort.Tensor('float32', preprocessedData,  [1, width, height, 3]);
+    const inputTensor = new ort.Tensor('float32', preprocessedData.data,  [1, width, height, 3]);
 
+    console.log(session.inputNames)
     const input = new String(session.inputNames[0]);
     const output = new String(session.outputNames[0]);
     
-    const feeds = {[input] : inputTensor};
+    //const feeds = {[input] : inputTensor};
+    const arrayImages = await preprocessBatch(imageSize,imageSize,'http://localhost:3000/getImages');
 
+    //console.log(arrayImages);
+
+    const feeds = {[input] : arrayImages};
+    start_time = performance.now();
     // Run model with Tensor inputs and get the result.
     const result = await session.run(feeds);
     
-    //Download result to use a python post process
-    onDownload(result);
+    // //Download result to use a python post process
+    // console.log(result);
 
-    //Print performance time
+    // console.log(result[output]);
+    onDownload(result,"output.json");
+
+    //Print performance timeimage.png
     console.log("Tiempo")
     console.log(performance.now() - start_time)
     
@@ -85,25 +96,8 @@ async function runExample() {
 /**
  * Preprocess raw image data to match YoloV4 requirement.
  */
-function debugSlice(){
-  const imageResized = ndarray(new Float32Array(5*10*4), [5,10, 4]);
-  ndarray.ops.assigns(imageResized, 1);
-  const imagePadded = ndarray(new Float32Array(10 * 10 * 3), [10, 10, 3]);
-  ndarray.ops.assigns(imagePadded, 0);
-  const dHeight = 2;
-  const dWidth = 0;
-  const newWidth = 10;
-  const newHeight = 5;
-  console.log(imagePadded.hi(dHeight+newHeight, dWidth+newWidth,null).lo(dHeight, dWidth, null).shape + " , " + imageResized.hi(null, null,3).shape);
-  console.log(imagePadded.hi(dHeight+newHeight, dWidth+newWidth,null).lo(dHeight, dWidth, null));
-  console.log(imageResized.hi(null, null,3));
 
-  ndarray.ops.assign(imagePadded.hi(dHeight+newHeight, dWidth+newWidth,null).lo(dHeight, dWidth, null), imageResized.hi(null, null,3));
-
-  console.log(imagePadded);
-
-}
-function preprocessYOLO(data, width, height, img) {
+function preprocessYOLO(width, height, img) {
 
     //width and height = iw, ih
 
@@ -115,6 +109,7 @@ function preprocessYOLO(data, width, height, img) {
     const scale = Math.min(height/realHeight,width/realWidth);
     const newWidth = parseInt(realWidth * scale);
     const newHeight = parseInt(realHeight * scale);
+    console.log(scale, newWidth, newHeight, realWidth, realHeight);
 
     //Creating a mat to use cv
     let mat = cv.imread(img);
@@ -138,5 +133,103 @@ function preprocessYOLO(data, width, height, img) {
 
     ndarray.ops.divseq(imagePadded, 255.0);
 
-    return imagePadded.data;
+    return imagePadded;
   }
+
+  async function preprocessBatch(yoloWidth, yoloHeight, url){
+    
+    const jsonData =await fetch(url)
+    .then(function(response) {
+      // The response is a Response instance.
+      // You parse the data into a useable format using `.json()`
+      return response.json();
+    }).then(async function(data) {
+      // `data` is the parsed version of the JSON returned from the above endpoint.
+      //console.log(data);  //
+      var imgArray = new Array();
+      var imgArrayNames = new Array();
+      var k = 0;
+      Object.entries(data).forEach(([key, value]) => {
+        //console.log(key, value);
+        //console.log(value["data"][0][0][0]);
+        var width = value["shape"][1],
+            height = value["shape"][0],
+            buffer = new Uint8ClampedArray(width * height * 4);
+        console.log(width,height);
+        for(var y = 0; y < height; y++) {
+          for(var x = 0; x < width; x++) {
+              var pos = (y * width + x) * 4; // position in buffer based on x and y
+              buffer[pos] = value["data"][y][x][2];           // some R value [0, 255]
+              buffer[pos+1] = value["data"][y][x][1];           // some G value
+              buffer[pos+2] = value["data"][y][x][0];           // some B value
+              buffer[pos+3] = 255;           // set alpha channel
+          }
+          //console.log("pase")
+        }
+                // create off-screen canvas element
+        var canvas = document.createElement('canvas'),
+        ctx = canvas.getContext('2d');
+
+        canvas.width = width;
+        canvas.height = height;
+
+        // create imageData object
+        var idata = ctx.createImageData(width, height);
+
+        // set our buffer as source
+        idata.data.set(buffer);
+
+        // update canvas with new data
+        ctx.putImageData(idata, 0, 0);
+
+        var dataUri = canvas.toDataURL(); // produces a PNG file
+        
+        const imgA = new Image(value["shape"][1],value["shape"][0]);
+        imgA.data = value["data"];
+        //console.log(img);
+
+        imgA.onload = function() {
+          imgA.width = this.width;
+          imgA.height = this.height;
+          }
+          imgA.src = dataUri
+          document.body.appendChild(imgA);
+        imgArray[k]=imgA;
+        imgArrayNames[k]=key; 
+        k = k + 1;
+      });
+
+      var countImages = k;
+      k = 0;
+      console.log(countImages);
+      const processArray = ndarray(new Float32Array(countImages*yoloHeight*yoloHeight*3), [countImages, yoloHeight, yoloHeight, 3]);
+      for (let i = 0; i < imgArray.length; i++) {
+        const element = imgArray[i];
+        console.log("Imagen: ",imgArrayNames[k]);
+        const preprocessedData = preprocessYOLO(yoloWidth, yoloHeight, element);
+        console.log(preprocessedData);
+        ndarray.ops.assign(processArray.pick(k,null,null,null), preprocessedData);
+      
+        // const input = new String(session.inputNames[0]);
+        // const output = new String(session.outputNames[0]);
+        // const inputTensor = new ort.Tensor('float32', preprocessedData.data,  [1, yoloWidth, yoloHeight, 3]);
+
+        // const feeds = {[input] : inputTensor};
+        // start_time = performance.now();
+        // const result = await session.run(feeds);
+        // //Print performance timeimage.png
+        // console.log("Tiempo")
+        // console.log(performance.now() - start_time)
+        // onDownload(result,imgArrayNames[k]+".json");
+        k = k + 1;
+      }
+      console.log(processArray);
+      const inputTensor = new ort.Tensor(processArray.data, [k, yoloHeight, yoloHeight, 3]);
+
+      console.log(k,inputTensor);
+      return inputTensor;
+    });
+    //console.log(jsonData);
+    return jsonData;
+  }
+
--- ONNX/server.js
+++ ONNX/server.js
@@ -1,9 +1,82 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: "500mb", extended: true, parameterLimit: 5000000 }))
 
+app.use(bodyParser.json({ limit: "500mb" }))
+// POST method route
+app.post('/postImages', (req, res) => {
+  var data = req.body;
+  global.imagesData = data;
+  console.log(data);
+  res.send("Data recibida");
+})
+
+app.get('/getImages', (req, res) => {
+  var data=global.imagesData;
+  console.log(data);
+  res.send(data);
+})
+
+//Api para experimentos
+app.post('/postImages1', (req, res) => {
+  var data = req.body;
+  global.imagesData1 = data;
+  console.log(data);
+  res.send("Data recibida");
+})
+app.post('/postImages2', (req, res) => {
+  var data = req.body;
+  global.imagesData2 = data;
+  console.log(data);
+  res.send("Data recibida");
+})
+app.post('/postImages3', (req, res) => {
+  var data = req.body;
+  global.imagesData3 = data;
+  console.log(data);
+  res.send("Data recibida");
+})
+app.post('/postImages4', (req, res) => {
+  var data = req.body;
+  global.imagesData4 = data;
+  console.log(data);
+  res.send("Data recibida");
+})
+app.post('/postImages5', (req, res) => {
+  var data = req.body;
+  global.imagesData5 = data;
+  console.log(data);
+  res.send("Data recibida");
+})
+app.get('/getImages1', (req, res) => {
+  var data=global.imagesData1;
+  console.log(data);
+  res.send(data);
+})
+app.get('/getImages2', (req, res) => {
+  var data=global.imagesData2;
+  console.log(data);
+  res.send(data);
+})
+app.get('/getImages3', (req, res) => {
+  var data=global.imagesData3;
+  console.log(data);
+  res.send(data);
+})
+app.get('/getImages4', (req, res) => {
+  var data=global.imagesData4;
+  console.log(data);
+  res.send(data);
+})
+app.get('/getImages5', (req, res) => {
+  var data=global.imagesData5;
+  console.log(data);
+  res.send(data);
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
